1a0510f181ac67f34e0cbeb2891d1e8270d0b433
roma_marshall <roma_marshall@outlook.com>
2022-03-21 21:53:31 +0100

    api setup

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,50 @@
 <template>
-  <div>
-
+  <div class="app">
+    <WeatherBoard />
+    <hr class="divider" />
   </div>
 </template>
 
 <script>
+import WeatherBoard from "@/components/WeatherBoard";
+
 export default {
   name: 'App',
-  components: {}
+  components: {
+    WeatherBoard
+  }
 }
 </script>
 
 <style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+.app {
+  text-align: center;
+  background: lightblue;
+  height: 700px;
+  width: 370px;
+  border-radius: 30px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.divider {
+  margin: 30px auto;
+  width: 50%;
+  background-color: #bebebe;
+  border: none;
+  border-radius: 3px;
+  height: 5px;
+  position: absolute;
+  left: 50%;
+  bottom: 0;
+  transform: translate(-50%, -50%);
+}
 </style>
---
+++ src/components/WeatherBoard.vue
@@ -0,0 +1,69 @@
+<template>
+  <div :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
+    <main>
+      <div class="search-box">
+        <input
+            type="text"
+            class="search-bar"
+            placeholder="Search"
+            v-model="query"
+            @keypress="fetchWeather"
+        />
+      </div>
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <div class="location-box">
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date">{{ dateBuilder() }}</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">{{ Math.round(weather.main.temp) }} Celium</div>
+          <div class="weather">{{ weather.weather[0].main }} Celium</div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      api_key: 'faced93f83dc78f3d4efd19048b7a0bf',
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
+    }
+  },
+  methods: {
+    fetchWeather(e) {
+      if (e.key === 'Enter') {
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+            .then(res => {
+              return res.json();
+            }).then(this.setResults);
+      }
+    },
+    setResults(results) {
+      this.weather = results;
+    },
+    dateBuilder() {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+.search-bar {
+  font-size: 16px;
+  width: 80%;
+  padding: 10px;
+  margin-top: 15px;
+  border: none;
+  border-radius: 10px;
+}
+
+.search-bar:focus {
+  outline: none;
+}
+</style>
