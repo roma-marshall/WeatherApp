dabce32a311d25c512d51dfd7d8aa407d24e4643
roma_marshall <roma_marshall@outlook.com>
2022-03-21 21:54:24 +0100

    dateBuilder added

--- src/components/WeatherBoard.vue
+++ src/components/WeatherBoard.vue
@@ -47,7 +47,15 @@ export default {
       this.weather = results;
     },
     dateBuilder() {
-      
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+      return `${day} ${date} ${month} ${year}`;
     }
   }
 }
